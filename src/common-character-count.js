a8df36ec2a9ad9a59d08c37ca9f2405011af6777
Dzmitry Halavach <kotik1@protonmail.com>
2022-10-11 16:06:49 +0300

    feat: add solutions to carbon dating, common character count, counting cats, and dream team tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
+function getCommonCharacterCount(s1, s2) {
+  if (s1.length <= s2.length) {
+    shorterString = s1;
+    longerString = s2;
+  } else {
+    shorterString = s2;
+    longerString = s1;
+  }
+
+  let arr1 = shorterString.split("");
+  let arr2 = longerString.split("");
+  let stack = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      stack.push(arr1[i]);
+      arr2.splice(arr2.indexOf(arr1[i]), 1);
+    }
+  }
+
+  //let result = new Set(stack);
+
+  return stack.length;
+}
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
